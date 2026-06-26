0515dd9090f564a3c3b90804c1148fc5027b427f
Nailt <probicevt@gmail.com>
2026-06-26 21:28:08 +0500

    fix tailwind styles and config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   base: '/MyPersonalsite.io/',
+  build: {
+    outDir: 'docs'
+  },
+  css: {
+    postcss: './postcss.config.js' // если есть
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
